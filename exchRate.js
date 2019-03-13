0f845711e380b3efa4aba2b1fdb2d1c3da173d2f
Christopher Brownlee <cdbreach@gmail.com>
2019-03-13 19:49:58 -0400

    Updating exchRate

--- exchRate.js
+++ exchRate.js
@@ -1,8 +1,15 @@
-let queryURL = 'https://api.exchangeratesapi.io/latest?base=USD'
+let rateURL = 'http://data.fixer.io/api/latest?access_key=315e444767a8513773b1071ac802f101&symbols&format=1';
 
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function(response){
-    console.log(response)
-})
+disExch();
+
+function disExch() {
+    $.ajax({
+        url: rateURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        let truBase = response.rates.USD;
+        console.log(truBase)
+        console.log(conCur)
+    });
+}

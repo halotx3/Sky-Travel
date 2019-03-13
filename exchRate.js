d4a19c2e45869e3f02c94dc050e15606fcee1fe4
Christopher Brownlee <cdbreach@gmail.com>
2019-03-13 18:27:21 -0400

    Cleared clutter from exchRate.js

--- exchRate.js
+++ exchRate.js
@@ -6,35 +6,3 @@ $.ajax({
 }).then(function(response){
     console.log(response)
 })
-
-let testURL = 'https://www.travel-advisory.info/api'
-
-$.ajax({
-    url: testURL,
-    method: 'GET',
-    dataType: 'json'
-}).then(function(response){
-    console.log(response)
-})
-
-// function createCORSRequest(method, url){
-//     var xhr = new XMLHttpRequest();
-//     if ("withCredentials" in xhr){
-//         xhr.open(method, url, true);
-//     } else if (typeof XDomainRequest != "undefined"){
-//         xhr = new XDomainRequest();
-//         xhr.open(method, url);
-//     } else {
-//         xhr = null;
-//     }
-//     return xhr;
-// }
-
-// let request = createCORSRequest("GET", testURL);
-// if (request){
-//     request.onload = function(){
-//         //do something with request.responseText
-//         console.log(request.responseText)
-//     };
-//     request.send();
-// }
